cc7017ff666d86c47a37ef94936cce26a53d9077
James Chambers <james@jameschambers.com>
2016-03-27 17:09:11 -0500

    Logging samples

---
+++ AspNetCoreLogging/AspNetCoreLogging/src/AspNetCoreLogging.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreLogging.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> _logger;
+
+        public HomeController (ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogError("These are not the frameworks you're looking for...");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult TestLogging()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCoreLogging/AspNetCoreLogging/src/AspNetCoreLogging/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Serilog;
+using GenFu;
+using System.Threading;
+
+namespace AspNetCoreLogging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()              
+              .WriteTo.File("log.txt")
+              .WriteTo.Seq("http://localhost:5341")
+              .CreateLogger();
+            
+            Log.Logger.Information("Executed at {ExecutionTime}", Environment.TickCount);
+
+            var people = A.ListOf<Person>(100);
+
+            people.ForEach(p =>
+                {
+                    Log.Logger.Information("{@Person} added at {EntryTime}", p, Environment.TickCount);
+                    Thread.Sleep(15);
+                });
+
+        }
+
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+}
